526a3d7bed08ab56ed9bc26f5cfe088cc0df2324
Tuấn Huỳnh Anh <huynhanhtuan8547@gmail.com>
2024-09-12 11:50:23 +0700

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import "./App.scss";
+import Header from "./components/Header";
+import TableUsers from "./components/TableUsers";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="app-container">
+      <Header />
+      <TableUsers />
     </div>
   );
 }
---
+++ src/components/TableUsers.js
@@ -0,0 +1,9 @@
+const TableUsers = () => {
+  return (
+    <div>
+      <h2>This is component TableUsers</h2>
+    </div>
+  );
+};
+
+export default TableUsers;
